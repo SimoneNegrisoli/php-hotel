7496c295b761d90bf743bc77413cd41b8d45ba85
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-29 15:56:24 +0100

    table

--- partials/Main.php
+++ partials/Main.php
@@ -6,7 +6,40 @@
     foreach ($hotels as $hotel) {
         echo '<div>' . $hotel['name'] . '' . $hotel['description'] . '' . $hotel['parking'] . '' . $hotel['vote'] . '' . $hotel['distance_to_center'] . '</div>';
     }
-
     ?>
+
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td scope="row">
+                        <?php echo $hotel['name'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['description'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['parking'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['vote'] ?>
+                    </td>
+                    <td>
+                        <?php echo $hotel['distance_to_center'] ?>
+                    </td>
+
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
 
 </main>
